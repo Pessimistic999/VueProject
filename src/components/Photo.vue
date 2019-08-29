bae672ed94482182163f15e33122eafac44c58f0
huangsf <1592753138@qq.com>
2019-08-29 10:11:35 +0800

    引入mui样式

---
+++ src/components/Photo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="photo">
+
+      <div class="photo">
+        <h1>图片页面</h1>
+        <van-tabs v-model="active">
+          <van-tab title="社会">
+            内容 1
+          </van-tab>
+          <van-tab title="科技">
+            内容 2
+          </van-tab>
+          <van-tab title="娱乐">
+            内容 3
+          </van-tab>
+          <van-tab title="生活">
+            内容 4
+          </van-tab>
+          <van-tab title="家居">
+            内容 5
+          </van-tab>
+        </van-tabs>
+      </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "photo",
+      data(){
+          return{
+            active: 1,
+          }
+      },
+      methods:{
+
+      },
+    }
+</script>
+
+<style scoped>
+  .photo{
+    width: 85%;
+    margin: auto;
+  }
+</style>
